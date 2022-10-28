1e981abfe4e3fa57ef5a2b10d5a4781ebc33ba2d
Nathan Bak <apollo@yellowslicker.com>
2022-10-28 14:05:21 -0700

    Add support for "required" tag

--- builder_error_test.go
+++ builder_error_test.go
@@ -9,13 +9,6 @@ import (
 
 func TestConfigBuilderErrors(t *testing.T) {
 
-	os.Setenv("MY_INT", "forty-two")
-	os.Setenv("MY_UINT", "-42")
-	os.Setenv("MY_FLOAT", "pi")
-	os.Setenv("MY_TIME", "1999")
-	os.Setenv("MY_DURATION", "3ly")
-	os.Setenv("MY_BOOL", "supposition")
-
 	tsts := []struct{ varName, varVal, expected string }{
 		{"MY_INT", "forty-two", `error reading "MY_INT" (strconv.ParseInt: parsing "forty-two": invalid syntax)`},
 		{"MY_UINT", "-42", `error reading "MY_UINT" (strconv.ParseUint: parsing "-42": invalid syntax)`},
@@ -23,6 +16,7 @@ func TestConfigBuilderErrors(t *testing.T) {
 		{"MY_TIME", "1999", `error reading "MY_TIME" (parsing time "1999" as "2006-01-02T15:04:05Z07:00": cannot parse "" as "-")`},
 		{"MY_DURATION", "3ly", `error reading "MY_DURATION" (time: unknown unit "ly" in duration "3ly")`},
 		{"MY_BOOL", "supposition", `error reading "MY_BOOL" (string "supposition" is not a valid boolean value)`},
+		{"NOT_MY_UINT", "123", `missing required var "MY_UINT"`},
 	}
 
 	for i, tst := range tsts {
